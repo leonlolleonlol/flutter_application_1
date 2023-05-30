3e15958c3978663a8a7ae720ba4a023e242a2ece
leonlolleonlol <leonkoj2004@gmail.com>
2023-05-29 22:17:37 -0400

    part 2 tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -37,13 +37,14 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;  
 
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-
+          BigCard(pair: pair),  
           // ↓ Add this.
           ElevatedButton(
             onPressed: () {
@@ -53,6 +54,36 @@ class MyHomePage extends StatelessWidget {
           ),
 
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+
+        // ↓ Make the following change.
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
